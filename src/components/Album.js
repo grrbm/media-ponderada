9c5f9aaba4aec553edd38486a6d1cbbb0f204c7e
Guilherme Reis <grrbm2@gmail.com>
2020-12-01 23:20:01 -0300

    first commit

---
+++ src/components/Album.js
@@ -0,0 +1,108 @@
+import React, {useState} from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import Main from './Main.js'
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        My Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export default function Album() {
+  const classes = useStyles();
+  const [average, setAverage] = useState(0);
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Média Ponderada
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Valor Médio:
+            </Typography>
+            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+              {average}
+            </Typography>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4} justify="center">
+            <Main 
+                average={average}
+                setAverage={setAverage}
+            />
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Essa página foi um teste.
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Teste realizado em 12/01/2020.
+        </Typography>
+        <Copyright />
+      </footer>
+      {/* End footer */}
+    </React.Fragment>
+  );
+}
---
+++ src/components/Main.js
@@ -0,0 +1,78 @@
+import React, {useState, useEffect} from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+  productsList: {
+      marginTop: 20
+  }
+}));
+
+export default function FormPropsTextFields({average, setAverage}) {
+  const classes = useStyles();
+  const [products, setProducts] = useState([]);
+
+  function addProduct(){
+    const productName = document.querySelector('#name-input').value;
+    const productPrice = document.querySelector('#price-input').value;
+    setProducts([...products,{productName, productPrice}]);
+  }
+  useEffect(()=>{
+      console.log("products list: "+JSON.stringify(products));
+      const map = new Map();
+      let sumWeights = 0;
+      let result = 0;
+      for(let i=0;i<products.length;i++){
+        map.set(products[i].productName,map.get(products[i].productName)+1 || 1);
+      }
+      console.log("THIS IS THE MAP: ");
+      console.log(map);
+
+      for (let key of map.keys()) {
+        let amount = map.get(key);
+        let price = products.filter((product)=> product.productName === key)[0].productPrice;
+        let term = amount * price;
+        console.log("term = "+amount+" x "+price);
+        sumWeights = sumWeights + Number(price);
+        result+=amount;
+      }
+      console.log("sum weights = "+sumWeights);
+      let avg = result/sumWeights;
+      setAverage(avg);
+
+  },[products])
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+        <TextField 
+            required 
+            id="name-input" 
+            label="Required" 
+        />
+        <TextField
+          id="price-input"
+          label="Price"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+        <Button onClick={()=>{addProduct()}} variant="contained" color="primary">
+            Add Product
+        </Button>
+        <div className={classes.productsList}>
+            {products.map((product)=><div>Product: {product.productName} - R$ {product.productPrice}</div>)}
+        </div>
+    </form>
+  );
+}
